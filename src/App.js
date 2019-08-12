2333fbc5adf4ca944630837c46b3a14c61bd429c
Rezials <suwapat.wow@gmail.com>
2019-08-13 00:07:06 +0700

    Create Board class and implements passing number and onChange() props to Cell instead of set state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,48 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Cell extends Component {
-  state = {
-    number: 1
-  };
   render() {
     return (
       <div
         onClick={e => {
           !this.props.isInitial &&
-            this.setState({
-              number: (this.state.number + 1) % 5
-            });
+            this.props.onChange((this.props.number + 1) % 5);
         }}
         className={`cell ${this.props.isInitial ? "initial" : ""}`}
       >
-        {this.state.number !== 0 && this.state.number}
+        {this.props.number !== 0 && this.props.number}
+      </div>
+    );
+  }
+}
+
+class Board extends Component {
+  state = {
+    board: [[1, 2, 3, 4], [3, 4, "", ""], [2, "", 4, ""], [4, "", "", 2]],
+    initial: [
+      [true, true, true, true],
+      [true, true, false, false],
+      [true, false, true, false],
+      [true, false, false, true]
+    ]
+  };
+  render() {
+    return (
+      <div className="Board">
+        {this.state.board.map((row, i) =>
+          row.map((number, j) => (
+            <Cell
+              key={`cell-${i}${j}`}
+              isInitial={this.state.initial[i][j]}
+              number={number}
+              onChange={newNumber => {
+                const board = this.state.board;
+                board[i][j] = newNumber;
+                this.setState({ board: board });
+              }}
+            />
+          ))
+        )}
       </div>
     );
   }
@@ -26,24 +53,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Board">
-          <Cell number={1} isInitial />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-          <Cell number={1} />
-        </div>
+        <Board />
       </div>
     );
   }
