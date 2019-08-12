1afed1de268d07d9363cb5472e486e0b0d0ecd4f
Rezials <suwapat.wow@gmail.com>
2019-08-12 21:55:21 +0700

    Add initial cell & only non-initial cells are clickable

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Cell extends Component {
+  state = {
+    number: 1
+  };
   render() {
-    return <div className="cell"> {this.props.number} </div>;
+    return (
+      <div
+        onClick={e => {
+          !this.props.isInitial &&
+            this.setState({
+              number: (this.state.number + 1) % 5
+            });
+        }}
+        className={`cell ${this.props.isInitial ? "initial" : ""}`}
+      >
+        {this.state.number !== 0 && this.state.number}
+      </div>
+    );
   }
 }
 
@@ -12,22 +27,22 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Board">
-          <Cell number={3} />
-          <Cell number={4} />
-          <Cell number={5} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
+          <Cell number={1} isInitial />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
+          <Cell number={1} />
         </div>
       </div>
     );
