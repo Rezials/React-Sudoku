4b878137143804d3ea3f0c191db0d6a80308deda
Rezials <suwapat.wow@gmail.com>
2019-08-13 00:30:53 +0700

    Implements submit button and validate answer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Cell extends Component {
+  onClick = () => {
+    if (!this.props.isInitial) {
+      this.props.onChange((this.props.number + 1) % 5);
+    }
+  };
+
   render() {
     return (
       <div
-        onClick={e => {
-          !this.props.isInitial &&
-            this.props.onChange((this.props.number + 1) % 5);
-        }}
+        onClick={this.onClick}
         className={`cell ${this.props.isInitial ? "initial" : ""}`}
       >
         {this.props.number !== 0 && this.props.number}
@@ -25,25 +28,38 @@ class Board extends Component {
       [true, true, false, false],
       [true, false, true, false],
       [true, false, false, true]
-    ]
+    ],
+    statusText: ""
   };
+
+  submit = () => {
+    const isValid = validate(this.state.board);
+    this.setState({
+      statusText: isValid ? "Board is complete!!" : "Board is invalid"
+    });
+  };
+
   render() {
     return (
-      <div className="Board">
-        {this.state.board.map((row, i) =>
-          row.map((number, j) => (
-            <Cell
-              key={`cell-${i}${j}`}
-              isInitial={this.state.initial[i][j]}
-              number={number}
-              onChange={newNumber => {
-                const board = this.state.board;
-                board[i][j] = newNumber;
-                this.setState({ board: board });
-              }}
-            />
-          ))
-        )}
+      <div>
+        <div className="Board">
+          {this.state.board.map((row, i) =>
+            row.map((number, j) => (
+              <Cell
+                key={`cell-${i}${j}`}
+                isInitial={this.state.initial[i][j]}
+                number={number}
+                onChange={newNumber => {
+                  const board = this.state.board;
+                  board[i][j] = newNumber;
+                  this.setState({ board: board });
+                }}
+              />
+            ))
+          )}
+        </div>
+        <button onClick={this.submit}>Submit</button>
+        <p>{this.state.statusText}</p>
       </div>
     );
   }
@@ -58,4 +74,28 @@ class App extends Component {
     );
   }
 }
+
+const validate = board => {
+  let isValid = true;
+  for (let i = 0; i < 4; i++) {
+    const horizontal = new Set();
+    const vertical = new Set();
+    const square = new Set();
+    for (let j = 0; j < 4; j++) {
+      horizontal.add(board[i][j]);
+      vertical.add(board[j][i]);
+      square.add(
+        board[2 * (i % 2) + (j % 2)][2 * Math.floor(i / 2) + Math.floor(j / 2)]
+      );
+    }
+    horizontal.delete(0);
+    vertical.delete(0);
+    square.delete(0);
+    if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+      isValid = false;
+    }
+  }
+  return isValid;
+};
+
 export default App;
